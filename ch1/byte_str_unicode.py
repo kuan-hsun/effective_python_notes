7b7f73d9dd142c76f5c879990a69c769392001ed
khcho <khcho@gmail.com>
2019-12-16 22:13:57 +0800

    add ch1 folder

---
+++ ch1/byte_str_unicode.py
@@ -0,0 +1,11 @@
+## 03 The difference between bytes, str, and unicode
+'''
+There are two types to represent characters in Python 3
+1. bytes: made by binary data (8 bit value)
+2. str: made by Unicode
+
+Represent Unicode into binary data, we need to encode, like UTF-8:
+Unicode -> encode -> Binary
+Binary -> decode -> Unicode
+
+'''
