1fd820006bcc9496a905c647afabe9869d422f7f
khcho <khcho@gmail.com>
2019-12-18 07:52:05 +0800

    Add 2 sample in byte_str_unicode.py

--- ch1/byte_str_unicode.py
+++ ch1/byte_str_unicode.py
@@ -9,11 +9,40 @@ Unicode -> encode -> Binary
 Binary -> decode -> Unicode
 
 '''
-
+import os
 
 def to_str(bytes_or_str):
-    if isinstance(bytes_or_str):
+    if isinstance(bytes_or_str, bytes):
         value = bytes_or_str.decode('utf-8')
     else:
         value = bytes_or_str
     return value
+
+def to_bytes(str_to_bytes):
+    if isinstance(str_to_bytes, str):
+        value = str_to_bytes.encode('utf-8')
+    else:
+        value = str_to_bytes
+    return value
+
+
+## EXAMPLE 1: always return str/bytes
+string_str = 'This is a sample string'
+print(type(string_str))
+print(to_str(string_str))
+print(to_bytes(string_str))
+
+string_bytes = b'This is a sample string'
+print(type(string_bytes))
+print(to_str(string_bytes))
+print(to_bytes(string_bytes))
+
+
+## EXAMPLE 2: file handles
+# 'wb' for write binary
+# 'rb' for read binary
+with open('./temp/random.bin', 'wb') as f:
+    f.write(string_bytes)
+
+with open('./temp/random.txt', 'w') as f:
+    f.write(string_str)
